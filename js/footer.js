8dad11315dca0f8a63df4c26bb20dde372d74981
hys990996 <hys990996@gmail.com>
2023-09-25 10:14:25 +0800

    modify style

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,18 @@
 let scrollTop = document.getElementById('scroll-top');
 scrollTop.addEventListener('click', function () {
 
+    // window.animate({
+    //     targets: document.scrollingElement,
+    //     duration: 500,
+    //     easing: 'linear',
+    //     scrollTop: 0
+    // });
+
     let html_el = document.getElementsByTagName("html")[0];
     html_el.scrollTo({
         top: 0,
         behavior: "smooth"
     });
-
     // window.scrollTo({
     //     top: 0,
     //     behavior: 'smooth'
