419b99282c4f46784010bcea9c43d0c6cf1b95df
hys990996 <hys990996@gmail.com>
2023-10-03 00:59:10 +0800

    modify header button style

--- js/header.js
+++ js/header.js
@@ -17,18 +17,19 @@ document.addEventListener('scroll', function () {
         })
     } else {
         header.classList.remove("bg-Grayishorange", "_underline")
-
-        headerBtn[0].setAttribute("src", "./images/common/cart.svg")
-        headerBtn[1].setAttribute("src", "./images/common/signin.svg")
         Array.from(liItem).forEach(element => {
             element.classList.remove("scroll")
         });
 
-        //判斷header是否包含-Light-grayish-orange
-        if (header.classList.contains("-Light-grayish-orange")) {
+        //判斷header是否包含Light-grayish-orange
+        if (header.classList.contains("Light-grayish-orange")) {
             logoImg.setAttribute("src", "./images/logo/logo_color.svg")
+            headerBtn[0].setAttribute("src", "./images/common/cart-orange.svg")
+            headerBtn[1].setAttribute("src", "./images/common/signin-orange.svg")
         } else {
             logoImg.setAttribute("src", "./images/logo/logo_white.svg")
+            headerBtn[0].setAttribute("src", "./images/common/cart.svg")
+            headerBtn[1].setAttribute("src", "./images/common/signin.svg")
         }
     }
 })
