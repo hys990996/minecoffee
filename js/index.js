6e6eae44cc8cf09e9cd5bc3bbbe7cd4741b1dede
hys990996 <hys990996@gmail.com>
2023-10-04 00:40:08 +0800

    add index journal effect

--- js/index.js
+++ js/index.js
@@ -1,36 +1,67 @@
 $(function () {
-    $('.carousel-item').eq(0).addClass('active');
-    var total = $('.carousel-item').length;
-    var current = 0;
+    $('.journal-item').eq(0).addClass('active');
+
+    let totalItem = $('.journal-item').length;
+    let currentItem = 0;
+    let prevItem = -1;
+
     $('#moveRight').on('click', function () {
-        var next = current;
-        current = current + 1;
-        setSlide(next, current);
+        prevItem = currentItem
+        currentItem = currentItem + 1
+
+        if (currentItem == totalItem) {
+            currentItem = 0
+        }
+
+        $('.journal-item').eq(prevItem).removeClass('active');
+        $('.journal-item').eq(currentItem).addClass('active');
+
     });
+
     $('#moveLeft').on('click', function () {
-        var prev = current;
-        current = current - 1;
-        setSlide(prev, current);
+
+        prevItem = currentItem
+        currentItem = currentItem - 1
+
+        if (currentItem == -1) {
+            currentItem = totalItem - 1
+        }
+
+        $('.journal-item').eq(prevItem).removeClass('active');
+        $('.journal-item').eq(currentItem).addClass('active');
     });
-    function setSlide(prev, next) {
-        var slide = current;
-        if (next > total - 1) {
-            slide = 0;
-            current = 0;
-        }
-        if (next < 0) {
-            slide = total - 1;
-            current = total - 1;
-        }
-        $('.carousel-item').eq(prev).removeClass('active');
-        $('.carousel-item').eq(slide).addClass('active');
-        setTimeout(function () {
-
-        }, 800);
 
 
 
-        console.log('current ' + current);
-        console.log('prev ' + prev);
-    }
+    // $('#moveRight').on('click', function () {
+    //     let nextItem = currentItem;
+    //     currentItem--;
+    //     setSlide(nextItem, currentItem);
+    // });
+
+    // $('#moveLeft').on('click', function () {
+    //     let preItem = currentItem;
+    //     currentItem++;
+    //     setSlide(preItem, currentItem);
+    // });
+
+    // function setSlide(prev, next) {
+    //     let slide = currentItem;
+
+    //     if (next > totalItem - 1) {
+    //         slide = 0;
+    //         currentItem = 0;
+    //     }
+
+    //     if (next < 0) {
+    //         slide = totalItem - 1;
+    //         currentItem = totalItem - 1;
+    //     }
+
+    //     $('.journal-item').eq(prev).removeClass('active');
+    //     $('.journal-item').eq(slide).addClass('active');
+
+    //     console.log('current ' + currentItem);
+    //     console.log('prev ' + prev);
+
 });
