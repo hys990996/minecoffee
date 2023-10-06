75e4b9e852062ad939a1e68890cbf9b8306e3a27
hys990996 <hys990996@gmail.com>
2023-10-06 22:46:00 +0800

    modify header rwd and add index rwd

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
 document.addEventListener('scroll', function () {
     let header = document.getElementById('header')
     let logoImg = document.getElementById('header-logo-img')
+    let logoImgM = document.getElementById('header-logo-img-m')
     let liItem = document.getElementsByClassName('nav-item')
     let headerBtn = document.querySelectorAll('div.header-button img')
     let hamburgerBtn = document.getElementsByClassName('hamburger-btn')[0]
@@ -8,10 +9,12 @@ document.addEventListener('scroll', function () {
 
     //判斷header不包含Light-grayish-orange
     if (!header.classList.contains("Light-grayish-orange")) {
+
         //滾動高度超過200時更換header樣式
         if (scrollHeight > 200) {
             header.classList.add("bg-Grayishorange", "_underline")
             logoImg.setAttribute("src", "./images/logo/logo_color.svg")
+            logoImgM.setAttribute("src", "./images/logo/logo_color.svg")
             headerBtn[0].setAttribute("src", "./images/common/cart-orange.svg")
             headerBtn[1].setAttribute("src", "./images/common/signin-orange.svg")
             Array.from(liItem).forEach(element => {
@@ -20,12 +23,13 @@ document.addEventListener('scroll', function () {
 
             hamburgerBtn.classList.add("Dark-orange")
 
-        } else {
+        } else { //滾動高度小於200時恢復header樣式
             header.classList.remove("bg-Grayishorange", "_underline")
             Array.from(liItem).forEach(element => {
                 element.classList.remove("scroll")
             });
             logoImg.setAttribute("src", "./images/logo/logo_white.svg")
+            logoImgM.setAttribute("src", "./images/logo/logo_white.svg")
             headerBtn[0].setAttribute("src", "./images/common/cart.svg")
             headerBtn[1].setAttribute("src", "./images/common/signin.svg")
 
