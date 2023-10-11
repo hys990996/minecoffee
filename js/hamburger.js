1d330226680845fa67ee9f91624c836dac622f75
hys990996 <hys990996@gmail.com>
2023-10-11 11:33:29 +0800

    journal content rwd and modify header rwd

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,29 +1,38 @@
 
 window.addEventListener('load', function () {
-    let btn_hamburger_el = document.getElementsByClassName("hamburger-btn")[0];
-    let nav_el = document.getElementsByClassName("header-block")[0];
+    let header = document.getElementById('header')
+    let btn_hamburger_el = document.getElementsByClassName("hamburger-btn")[0]
+    let nav_el = document.getElementsByClassName("header-block")[0]
     let logoImgM = document.getElementById('header-logo-img-m')
     let headerBtn = document.querySelectorAll('div.header-button img')
 
     btn_hamburger_el.addEventListener("click", function () {
+
         this.classList.toggle("-on")
         nav_el.classList.toggle("show")
 
-        if (btn_hamburger_el.classList.contains("-on")) {
-            header.classList.add("bg-Grayishorange")
-            logoImgM.setAttribute("src", "./images/logo/logo_color.svg")
-            headerBtn[0].setAttribute("src", "./images/common/cart-orange.svg")
-            headerBtn[1].setAttribute("src", "./images/common/signin-orange.svg")
+        let scrollHeight = window.scrollY
 
-            btn_hamburger_el.classList.add("Dark-orange")
-        } else {
-            header.classList.remove("bg-Grayishorange")
-            logoImgM.setAttribute("src", "./images/logo/logo_white.svg")
-            headerBtn[0].setAttribute("src", "./images/common/cart.svg")
-            headerBtn[1].setAttribute("src", "./images/common/signin.svg")
+        if (header.classList.contains("-indexpage") && scrollHeight <= 200) {
 
-            btn_hamburger_el.classList.remove("Dark-orange")
+            if (btn_hamburger_el.classList.contains("-on")) {
+                header.classList.add("bg-Grayishorange")
+                logoImgM.setAttribute("src", "./images/logo/logo_color.svg")
+                headerBtn[0].setAttribute("src", "./images/common/cart-orange.svg")
+                headerBtn[1].setAttribute("src", "./images/common/signin-orange.svg")
+
+                btn_hamburger_el.classList.add("Dark-orange")
+            } else {
+                header.classList.remove("bg-Grayishorange")
+                logoImgM.setAttribute("src", "./images/logo/logo_white.svg")
+                headerBtn[0].setAttribute("src", "./images/common/cart.svg")
+                headerBtn[1].setAttribute("src", "./images/common/signin.svg")
+
+                btn_hamburger_el.classList.remove("Dark-orange")
+
+            }
         }
+
     });
 
 })
